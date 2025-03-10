eab34af351bc5d39be036d8f118e7833368ae350
computerdev21 <devchetal@gmail.com>
2025-03-10 06:16:28 -0400

    Config fixes

--- next.config.js
+++ next.config.js
@@ -1,41 +1,37 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  serverRuntimeConfig: {
-    PROJECT_ROOT: __dirname,
-  },
-  images: {
-    domains: [
-      "localhost",
-      "vercel.app",
-      "ayedot.com",
-      "ayex8storex9.s3.ap-south-1.amazonaws.com",
-      "images.ctfassets.net",
-      "uploads.toptal.io",
-      "www.browserstack.com",
-      "miro.medium.com",
-      "hkandala.dev",
-      "res.cloudinary.com",
-      "img.icons8.com",
-      "64.media.tumblr.com",
-      "pbs.twimg.com",
-      "i.pinimg.com",
-      "media4.giphy.com",
-      "img.icons8.com",
-      "steamuserimages-a.akamaihd.net",
-      "c.tenor.com",
-      "thumbs.gfycat.com",
-      "giffiles.alphacoders.com",
-      "j.gifs.com",
-      "i.pinimg.com",
-      "i.makeagif.com",
-      "i.ibb.co",
-      "bourhaouta.gallerycdn.vsassets.io",
-      "upload.wikimedia.org",
-      "encrypted-tbn0.gstatic.com"
-    ],
-  }
+    reactStrictMode: true,
+    typescript: {
+        ignoreBuildErrors: false,
+    },
+    serverRuntimeConfig: {
+        PROJECT_ROOT: __dirname,
+    },
+    images: {
+        domains: [
+            "localhost",
+            "vercel.app",
+            "ayedot.com",
+            "ayex8storex9.s3.ap-south-1.amazonaws.com",
+            "images.ctfassets.net",
+            "uploads.toptal.io",
+            "www.browserstack.com",
+            "miro.medium.com",
+            "hkandala.dev",
+            "res.cloudinary.com",
+            "img.icons8.com",
+            "64.media.tumblr.com",
+            "pbs.twimg.com",
+            "i.pinimg.com",
+            "media4.giphy.com",
+            "i.makeagif.com",
+            "i.ibb.co",
+            "bourhaouta.gallerycdn.vsassets.io",
+            "upload.wikimedia.org",
+            "encrypted-tbn0.gstatic.com",
+            "cdn.worldvectorlogo.com",
+            "groq.com",
+            "numpy.org"
+        ],
+    },
 };
