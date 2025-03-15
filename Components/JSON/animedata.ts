e4238fa47720404384e55fbe8f9090071c996bad
computerdev21 <devchetal@gmail.com>
2025-03-15 07:31:54 -0400

    animeData

--- Components/JSON/animedata.ts
+++ Components/JSON/animedata.ts
@@ -5,6 +5,11 @@ export const animedata = () => [
     subtitle: "720 Episodes",
   },
   {
+    name: "One Piece",
+    subtitle: "1000 plus Episode : Epic pirate adventure",
+    gif: "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExejd6YWRhYWhkMnJmNml4MHlpd3o4ZmF5ZjNhcjlqNjV0NXg1bG9maiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/4OV1bLOIWwIXRxpXlN/giphy.gif"
+  },
+  {
     img: "https://c.tenor.com/bnRtrO9OLyIAAAAd/my-hero-academia-world-heroes-mission.gif",
     title: "My hero academia ",
     subtitle: "5 Seasons",
@@ -20,7 +25,7 @@ export const animedata = () => [
     subtitle: "On Going",
   },
   {
-    img: "https://thumbs.gfycat.com/BabyishOilyIchneumonfly-max-1mb.gif",
+    img: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHlkNGI5MTBlNTA2bXkycWw3Z3d6dXRvZHVnYWR2cjJqOGZhcmxwMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/zkNBtlymM6zX4DndrU/giphy.gif",
     title: "Jujutsu Kaisen",
     subtitle: "On Going",
   },
@@ -43,5 +48,25 @@ export const animedata = () => [
     img: "https://i.makeagif.com/media/10-24-2015/wKS2sZ.gif",
     title: "Beyblade",
     subtitle: "3 seasons",
+  },
+  {
+    name: "Demon Slayer",
+    subtitle: "A tale of demons and a brave warrior",
+    gif: "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExeTN4Z3Mwb3A4cHlubjZmc2Q4MzJ0ZHcxODRveXNkcHB2dGZ1cjZwMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8wGB4kWDNIYT3OHQqD/giphy.gif"
+  },
+  {
+    name: "Haikyu!!",
+    subtitle: "High-flying volleyball action",
+    gif: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGYyb3BtbW5ybHRsaWZsYzhzdG0wOXM5NHFhbmhzaXBtbjR6MmVmaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Xd3xIZ9XHO5uE/giphy.gif"
+  },
+  {
+    name: "Solo Leveling",
+    subtitle: "A leveling up adventure",
+    gif: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjdodGp1ZTlibDRsaGRvZGF1bm1hbXI0dzFoa3BtYTVvZDUxYTBoaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ySvhFxq6Z4LrbqaikJ/giphy.gif"
+  },
+  {
+    name: "Attack on Titan",
+    subtitle: "A fight for survival against gigantic foes",
+    gif: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExenpmeGprem5nemUxaXB2ZzNmbXBrMHVkMW91dW8zM2JraXc5bWdxcyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7btQ8jDTPGDpgc6I/giphy.gif"
   },
 ];
